4e5af662e3c75a2842f396efad1a478edb66aa34
awolu owolabi samuel <58808750+Samuel3310@users.noreply.github.com>
2023-07-06 08:13:47 +0100

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,23 @@
+const login = document.querySelector(".login");
+
+login.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const invalid = document.querySelector(".invalid");
+  const names = login.username.value;
+  const password = login.password.value;
+
+  let getUser = JSON.parse(localStorage.getItem(names));
+
+
+
+  if (getUser.name !== names && getUser.password !== password) {
+    invalid.textContent = "Error Username and Password";
+  } else {
+    
+  
+      sessionStorage.setItem("user", JSON.stringify(getUser));
+      window.location.href = "./dashboard/index.html";
+    
+  }
+});
---
+++ js/main.js
@@ -0,0 +1,52 @@
+(function ($) {
+  "use strict";
+
+  $(".toggle-password").click(function () {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
+})(jQuery);
+
+const log = console.log;
+
+let submit = document.querySelector(".submit");
+
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const username = submit.names.value;
+  const email = submit.email.value;
+  const address = submit.address.value;
+  const password = submit.password.value;
+
+  //   const nameErr = document.querySelector('.name_err');
+  //   const emailErr = document.querySelector('.email_err');
+  //   const addressErr = document.querySelector('.address_err');
+  //   const passwordErr = document.querySelector('.password_err');
+
+  //   if(!passwordErr){
+
+  //   passwordErr.textContent = 'Password required';
+  // }
+  let getUsers = JSON.parse(localStorage.getItem(username));
+
+  if (getUsers) {
+    alert("Username already in use");
+  } else {
+    const users = {
+      name: username,
+      email: email,
+      password: password,
+      address: address,
+    };
+
+    let setItem = localStorage.setItem(username, JSON.stringify(users));
+
+    alert("succesfully registered");
+    window.location.href = "./login.html";
+  }
+});
